0a0f3ee46506429449eedc5eb81992f1e59b3aff
psj45633 <psj45633@gmail.com>
2026-04-20 21:21:44 +0900

    Initial commit

---
+++ Assets/01_Scripts/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private Vector2 inputVec;
+    [SerializeField] private float speed;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVec);
+    }
+
+    private void LateUpdate()
+    {
+        if (Mathf.Abs(inputVec.x) < 0.01f) return;
+
+        sr.flipX = inputVec.x < 0;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Survivor_
+
